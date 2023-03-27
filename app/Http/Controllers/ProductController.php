5847975859565021e2f4daa1dedfc9da671ae54e
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-03-27 16:25:22 +0900

    updateアクション

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -77,7 +77,10 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->update();
     }
 
     /**
