0f309dfb03562164893e51c3d6fd5d1d04362a3b
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-04-02 17:34:11 +0900

    お気に入り機能が使えるようにお気に入りする商品とユーザーの紐づけを行う

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,12 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use App\Notifications\CustomVerifyEmail;
 use App\Notifications\CustomResetPassword;
+use Overtrue\LaravelFavorite\Traits\Favoriter;
 
 //class User extends Authenticatable implements MustVerifyEmail
   class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, Favoriter;
 
     public function sendEmailVerificationNotification()
     {
