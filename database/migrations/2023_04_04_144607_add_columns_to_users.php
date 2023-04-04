c894d19d44108aa2c473b9774f6caf8148c744a9
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-04-04 14:51:28 +0900

    データベースに新しいカラムを追加

--- database/migrations/2023_04_04_144607_add_columns_to_users.php
+++ database/migrations/2023_04_04_144607_add_columns_to_users.php
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('postal_code')->default('');
+            $table->text('address');
+            $table->string('phone')->default('');
         });
     }
 
