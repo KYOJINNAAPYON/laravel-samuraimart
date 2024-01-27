cc1d59c71f71f14261044a4760d4c807b6758315
Kyojinnaapyon <kyojinnaapyon@gmail.com>
2024-01-27 10:44:38 +0900

    CSVインポートに伴って修正

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -118,27 +118,27 @@ class ProductController extends AdminController
         });
 
         $lexer->parse($file, $interpreter);
-         foreach ($rows as $key => $value) {
- 
-             if (count($value) == 7) {
-                 Product::create([
-                     'name' => $value[0],
-                     'description' => $value[1],
-                     'price' => $value[2],
-                     'category_id' => $value[3],
-                     'image' => $value[4],
-                     'recommend_flag' => $value[5],
-                     'carriage_flag' => $value[6],
-                 ]);
-             }
-         }
- 
-         return response()->json(
-             ['data' => '成功'],
-             200,
-             [],
-             JSON_UNESCAPED_UNICODE
-         );
-     }
+        foreach ($rows as $key => $value) {
+
+            if (count($value) == 7) {
+                Product::create([
+                    'name' => $value[0],
+                    'description' => $value[1],
+                    'price' => $value[2],
+                    'category_id' => $value[3],
+                    'image' => $value[4],
+                    'recommend_flag' => $value[5],
+                    'carriage_flag' => $value[6],
+                ]);
+            }
+        }
+
+        return response()->json(
+            ['data' => '成功'],
+            200,
+            [],
+            JSON_UNESCAPED_UNICODE
+        );
+    }
     
 }
